77b8df1a88cd9a10acb2e9d08b1d04775f4f1ff1
Malone <manueljavier.martinez@gmail.com>
2020-12-26 17:48:24 -0600

    routing

--- recipe-book/src/app/header/header.component.ts
+++ recipe-book/src/app/header/header.component.ts
@@ -1,16 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{
-    
-    @Output() selectedPartToOutside = new EventEmitter<{section: string}>();
+export class HeaderComponent{    
 
-    selectPart(part){
-        this.selectedPartToOutside.emit({
-            section: part
-        });
-    }
 }
