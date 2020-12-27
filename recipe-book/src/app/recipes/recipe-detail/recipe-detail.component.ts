cbcc54e94e996a028b999eddbbac285d2a368bca
Malone <manueljavier.martinez@gmail.com>
2020-12-27 17:16:00 -0600

    routing

--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,12 +11,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
 
-  @Input() recipe: Recipe;
-
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService, 
+    private activeRouter: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.activeRouter.params.subscribe(
+      (params: Params) => {
+        this.recipe = this.recipeService.getRecipe(+params['id']);
+      }
+    );
   }
 
   addToShoppingList(ingredients: Ingredient[]){
--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -7,24 +7,31 @@ export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('Custard Pie', 'You can eat pie', 
+        new Recipe(1, 'Custard Pie', 'You can eat pie', 
         'https://images-gmi-pmc.edge-generalmills.com/9a8a63c9-959d-4565-a0f1-27bc958167d5.jpg', 
         [
-            new Ingredient('Frutas', 3)
+            new Ingredient('Frutas', 3),
+            new Ingredient('Leche', 1)
         ]),
-        new Recipe('Fried Chicken', 'Grab a chicken leg', 
+        new Recipe(2, 'Fried Chicken', 'Grab a chicken leg', 
         'https://www.eatwell101.com/wp-content/uploads/2019/08/tuscan-salmon-recipe.jpg',
         [
-            new Ingredient('Pollo', 1)
+            new Ingredient('Pollo', 1),
+            new Ingredient('Chile verde', 5)
         ]),
-        new Recipe('Potato Salad', 'Side this', 
+        new Recipe(3, 'Potato Salad', 'Side this', 
         'https://images-gmi-pmc.edge-generalmills.com/b4856949-30a8-42d9-bec7-e061ace4d00e.jpg',
         [
-            new Ingredient('Papas', 10)
+            new Ingredient('Papas', 10),
+            new Ingredient('Salsa de tomate', 2)
         ])
       ];
 
       getRecipes(){
           return this.recipes.slice();
+      }
+
+      getRecipe(id: number){
+          return this.recipes.find( recipe => recipe.id === id );
       }
 }
