7f8434775ba7dfa3ac9009da510a7ac6620b6071
Malone <manueljavier.martinez@gmail.com>
2020-12-27 09:15:26 -0600

    second-commit-services-dependency

--- recipe-book/src/app/shopping-list/shopping-list.service.ts
+++ recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,28 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
 
 export class ShoppingListService{
-    logShoppingList(list: string){
-        console.log('The list is ' + list);
+
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatos', 10)
+    ];
+
+    getIngredients(){
+        return this.ingredients.slice();
     }
+
+    addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    addIngredientList(ingredients: Ingredient[]){
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
 }
